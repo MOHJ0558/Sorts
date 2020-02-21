09d11b38c816b8a8248d616baa0bb3a884f1b302
Mo Haojun <3155611183@qq.com>
2020-02-21 08:09:26 +0800

    Update and rename SelectionSort.c to SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void SLS(double arr[], int len){
+	int minidx, temp;
+	for(int i = 0; i<len-1;i++){
+	    minidx = i;
+	    for(int j = i+1; j<len; j++){
+		    if(arr[j]<arr[minidx])
+				minidx = j;
+		}
+		temp = arr[i];
+		arr[i] = arr[minidx];
+		arr[minidx] = temp;
+	}
+}
