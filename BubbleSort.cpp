b5bc1dd5430282db3ce0672a8798ab5d2f1d16f4
Mo Haojun <3155611183@qq.com>
2020-02-21 08:10:58 +0800

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void BBS(double arr[], int len){
+	for(int i = 0; i < len - 1; i++){//轮数 
+		for(int j = 0; j < len - i - 1; j++){//比较次数 
+			if(arr[j] >= arr[j+1]){
+				double temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			} 
+		}
+	}
+}
